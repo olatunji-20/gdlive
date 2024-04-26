8fb7f50bed2b31671ef810ed0d87e34282f6fcee
falana sheriff <falanasheriffdeen@gmail.com>
2024-04-26 14:21:09 +0100

    continue

--- src/components/Stories.vue
+++ src/components/Stories.vue
@@ -4,7 +4,7 @@
             <div class="rounded-md w-[auto] h-[auto] p-0.5 bg-gray-50 flex flex-row flex-nowrap">
                 <div class="w-[30px] h-[30px] cursor-pointer text-center pt-0.5"><i class="material-symbols-outlined font-bold">search</i></div>
                 <div class="h-[30px] w-[350px] ml-2">
-                    <input placeholder="search name, country or region" class="bg-transparent w-[100%] h-[100%] text-xs indent-2" />
+                    <input placeholder="search name, country or region" class="bg-transparent w-[100%] h-[100%] text-xs border-0 indent-2 focus:outline-none focus:border rounded-md border-gray-300" />
                 </div>
             </div> 
             <div class="bg-green-400 w-[100px] h-[30px] text-center">
